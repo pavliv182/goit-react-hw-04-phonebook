fc405ae190b982c3c62b37eb621ee0340309bddd
pavliv182 <uptn182@gmail.com>
2022-10-04 14:32:13 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Notify } from 'notiflix';
 import Section from './Section';
 import Phonebook from './Phonebook';
@@ -6,82 +6,75 @@ import Contacts from './Contacts';
 import Filter from './Filter';
 import Notification from './Notification';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  const firstRender = useRef(true);
+  console.log(firstRender);
+
+  useEffect(() => {
     const contacts = JSON.parse(localStorage.getItem('contacts'));
     console.log(contacts);
     //  если const contacts существует и у него есть длинна то:
     if (contacts?.length) {
-      this.setState({ contacts: contacts });
+      setContacts(contacts);
     }
-  }
+    firstRender.current = false;
+  }, []);
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts.length !== prevState.contacts.length) {
-      const newContacts = JSON.stringify(this.state.contacts);
-      localStorage.setItem('contacts', newContacts);
+  useEffect(() => {
+    if (!firstRender.current) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-  }
+  }, [contacts]);
 
-  addContact = data => {
-    if (this.state.contacts.find(el => el.name === data.name)) {
+  const addContact = data => {
+    if (contacts.find(el => el.name === data.name)) {
       Notify.failure('This contact is already in phonebook');
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data],
-    }));
+    setContacts(prevState => [...prevState, data]);
 
     Notify.success('Contact added succesfully!');
   };
 
-  addFilter = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  const addFilter = e => {
+    setFilter(e.target.value);
   };
 
-  filterContacts = () => {
-    if (this.state.filter) {
-      const filteredContacts = this.state.contacts.filter(el =>
-        el.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const filterContacts = () => {
+    if (filter) {
+      const filteredContacts = contacts.filter(el =>
+        el.name.toLowerCase().includes(filter.toLowerCase())
       );
       return filteredContacts;
     }
 
-    return this.state.contacts;
+    return contacts;
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts.filter(el => el.id !== id)],
-    }));
+  const deleteContact = id => {
+    setContacts(prevState => [...prevState.filter(el => el.id !== id)]);
   };
 
-  render() {
-    const { addContact, addFilter, filterContacts, deleteContact } = this;
-    return (
-      <>
-        <Section title="Phonebook">
-          <Phonebook addContact={addContact} />
-        </Section>
-        <Section title="Contacts">
-          {this.state.contacts.length ? (
-            <>
-              <Filter addFilter={addFilter} value={this.state.filter} />
-              <Contacts data={filterContacts()} deleteContact={deleteContact} />
-            </>
-          ) : (
-            <Notification message="Add new contact" />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+  // const { addContact, addFilter, filterContacts, deleteContact } = this;
+  return (
+    <>
+      <Section title="Phonebook">
+        <Phonebook addContact={addContact} />
+      </Section>
+      <Section title="Contacts">
+        {contacts.length ? (
+          <>
+            <Filter addFilter={addFilter} value={filter} />
+            <Contacts data={filterContacts()} deleteContact={deleteContact} />
+          </>
+        ) : (
+          <Notification message="Add new contact" />
+        )}
+      </Section>
+    </>
+  );
+};
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,79 +1,73 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import id from 'bson-objectid';
 import PropTypes from 'prop-types';
 
 import './phonebook.module.css';
 
-class Phonebook extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = e => {
-    const name = e.target.name;
-    const number = e.target.value;
-    this.setState({
-      [name]: number,
+const Phonebook = ({ addContact }) => {
+  const [formData, setFormData] = useState({ name: '', number: '' });
+
+  const { name, number } = formData;
+
+  const handleChange = e => {
+    setFormData(prevState => {
+      return { ...prevState, [e.target.name]: e.target.value };
     });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const name = this.state.name;
-    const number = this.state.number;
     const data = {
       name,
       number,
       id: id(),
     };
-    this.props.addContact(data);
-    this.formReset();
+    addContact(data);
+    formReset();
   };
 
-  formReset = () => {
-    this.setState({
+  const formReset = () => {
+    setFormData({
       name: '',
       number: '',
     });
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="Name">
-            Name
-            <input
-              id="Name"
-              value={name}
-              onChange={this.handleChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label htmlFor="Number">
-            Number
-            <input
-              id="Number"
-              value={number}
-              onChange={this.handleChange}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-          <button type="submit">Add contact</button>
-        </form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Name">
+          Name
+          <input
+            id="Name"
+            value={name}
+            onChange={handleChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label htmlFor="Number">
+          Number
+          <input
+            id="Number"
+            value={number}
+            onChange={handleChange}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    </>
+  );
+};
+
 export default Phonebook;
 
 Phonebook.propTypes = {
